3aa24c3020ea6668a194cfe5b8d3d30fd1c3c959
Bernardo Mendes <bernardobittencourtmendes@gmail.com>
2025-11-07 21:12:35 -0300

    separating amount of characters for each char_type

--- main.go
+++ main.go
@@ -6,13 +6,16 @@ import (
 	"math/big"
 )
 
-const UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZÇ"
-const LOWER = "abcdefghijklmnopqrstuvwxyzç"
-const NUMBER = "0123456789"
-const SPECIAL = "!@#$%&*?<>"
+const (
+	UPPER      = "ABCDEFGHIJKLMNOPQRSTUVWXYZÇ"
+	LOWER      = "abcdefghijklmnopqrstuvwxyzç"
+	NUMBER     = "0123456789"
+	SPECIAL    = "!@#$%&"
+	MAX_LENGHT = 32
+)
 
-func generate_rand() int64 {
-	r, _ := rand.Int(rand.Reader, big.NewInt(100))
+func generate_rand(cap int64) int64 {
+	r, _ := rand.Int(rand.Reader, big.NewInt(cap))
 	return r.Int64()
 }
 
@@ -21,10 +24,26 @@ func main() {
 		32 characters (UPPER, lower, special, number)
 		Random:
 			- How many characters of each type will exist in the password (at least 1 of each)
-			- Choose freely from each array the chosen characters
+			- Choose randomly from each array the chosen characters
 			- Mix these characters
 	*/
+	var remaining_chars int64 = MAX_LENGHT
+	var n_upper_chars int64
+	var n_lower_chars int64
+	var n_special_chars int64
+	var n_number_chars int64
 
-	random1 := generate_rand()
-	fmt.Println(random1)
+	n_upper_chars = generate_rand(remaining_chars)
+	remaining_chars -= n_upper_chars
+	n_lower_chars = generate_rand(remaining_chars)
+	remaining_chars -= n_lower_chars
+	n_special_chars = generate_rand(remaining_chars)
+	n_number_chars = remaining_chars
+	if remaining_chars == 0 {
+		n_special_chars--
+		n_number_chars = remaining_chars + 1
+	}
+
+	fmt.Printf("Upper: %d, Lower: %d, Special: %d, Number: %d\n",
+		n_upper_chars, n_lower_chars, n_special_chars, n_number_chars)
 }
